888642873eed90fd70fb8fe9219ec5075636bfc4
prasad-bigdp <bigdp585@gmail.com>
2024-05-30 07:52:51 +0530

    first commit

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <p className=" text-3xl text-center text-black hover:text-red-600">Hello worrld</p>
+    </>
+  )
+}
+
+export default App
