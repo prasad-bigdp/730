9c6d0a3c5ef4bbe3b9b6afcbf1216d1d0a508050
prasad-bigdp <bigdp585@gmail.com>
2024-05-31 08:18:30 +0530

    project

--- ecommerce/src/App.jsx
+++ ecommerce/src/App.jsx
@@ -9,10 +9,25 @@ import Layout from './Layout'
 function App() {
 
   return (
-    <>
-     <p className=" text-3xl text-center text-black hover:text-red-600">Hello worrld</p>
-    </>
-  )
+		<Layout>
+			<Router>
+				<Routes>
+					<Route
+						path='/'
+						Component={Home}></Route>
+					<Route
+						path='/Product/:id'
+						Component={Product}></Route>
+					<Route
+						path='/Products'
+						Component={Products}></Route>
+					<Route
+						path='/Cart'
+						Component={Cart}></Route>
+				</Routes>
+			</Router>
+		</Layout>
+	)
 }
 
 export default App
